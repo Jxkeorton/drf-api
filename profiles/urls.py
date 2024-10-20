83aa534a7c5bf8a08c53d2bf78e8ed9b7c3552e2
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-10-20 18:05:18 +0000

    Add permissions to api calls

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
-    path('profile/<int:pk>/', views.PerofileDetail.as_view()),
+    path('profile/<int:pk>/', views.ProfileDetail.as_view()),
 ]
