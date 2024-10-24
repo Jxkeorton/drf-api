4003b37c4dcfb0daa17e0b00e1f6c7bd1fad12c5
Jxkeorton <jakeorton99@gmail.com>
2024-10-24 21:22:45 +0100

    Add allowed host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drfapi-code-institute.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'drfapi-code-institute-76c4941a10d3.herokuapp.com']
 
 # Application definition
 
